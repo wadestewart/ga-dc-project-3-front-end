7cce09297041ab33ec3c7b52d362ffa811cecf94
Wade Stewart <stew.wade@gmail.com>
2018-01-30 16:39:54 -0500

    CR functionality, broke out Movie component

--- src/App.js
+++ src/App.js
@@ -4,77 +4,97 @@ import axios from 'axios'
 
 class App extends Component {
   state = {
-        movies: [],
+      movies: [],
+      newMovie: {
         movieValue: '',
         yearValue: ''
-    }
+      },
+      currentMovie: {
+        movieValue: '',
+        yearValue: ''
+      },
+  }
 
-componentDidMount () {
-	axios.get('https://sheltered-dawn-94402.herokuapp.com/movies')
-    .then(response => {this.setState({ movies: response.data })
-    console.log(response)})
-}
+  componentDidMount = () => {
+    axios.get('https://sheltered-dawn-94402.herokuapp.com/movies')
+      .then(response => {
+        console.log(response)
+        this.setState({ movies: response.data })
+    })
+  }
 
-handleChange = (event) => {
-    this.setState({ movieValue: event.target.movieValue })
-    this.setState({ yearValue: event.target.yearValue })
+  handleChange = (event) => {
+    let newState = {}
+    newState[event.target.name] = event.target.value
+    // this.setState(newState, _ => console.log(this.state))
+    this.setState(newState)
+  }
 
-}
-
-handleSubmit = (event) => {
+  handlePost = (event) => {
     console.log('A movie was submitted: ' + this.state.movieValue)
     console.log('A year was submitted: ' + this.state.yearValue)
+    let sendName = this.state.movieValue
+    let sendYear = Number(this.state.yearValue)
     event.preventDefault()
     axios
-        .post('https://sheltered-dawn-94402.herokuapp.com/movies', {movie: { name: this.state.movieValue, year: this.state.yearValue }})
-        .then(response => {
-            console.log(response)
-            this.setState({ movieValue: this.state.movieValue })
-            this.setState({ yearValue: this.state.yearValue })
+      .post(
+        'https://sheltered-dawn-94402.herokuapp.com/movies', 
+        { name: sendName, year: sendYear, providers: "Netflix, HBO Now" }
+      )
+      .then(response => {
+          console.log('response from POST', response.data)
+          // the response from the POST request handling method on the express side - REQ.BODY
+          // the response will be the thing just created in the db
+          this.setState(prevState => {
+            return {
+              movies: [...prevState.movies, response.data]
+            }
+          })
+      })
+  }
 
-        })
-}
-
-render() {
-  let movies = this.state.movies.map((movie, index) => {
-      return <div className='center'>
-              {/* <li key={index} className="movie-name">
-                  <a href={'/movie/' + movie.name}>{movie.name}</a>
-              </li> */}
+  render() {
+    let movies = this.state.movies.map((movie, index) => {
+      return <div key={index} className='center'>
               <h4 className="movie-name">{movie.name}</h4>
               <p className="movie-year">{movie.year}</p>
               <p className="movie-provider">{movie.provider}</p>
           </div>
-})
-  return <div className="App">
-          <nav className="pink accent-2">
-              <div className="nav-wrapper">
-                  <a href="/" className="brand-logo center">
-                      Netflix, Etc. & Chill
-                  </a>
-                  <ul className="right hide-on-med-and-down">
-                      <li>
-                          <a href="/create ">Create</a>
-                      </li>
-                  </ul>
-              </div>
-          </nav>
-          <main>
-              <form action="/results" method="get">
-                  <input type="text" placeholder="Search" className="input" />
-                  <button className="button">Submit</button>
-              </form>
-              {movies}
-              <h4 className="center">Add a movie!</h4>
+    })
 
-              <form className="center" action="/movies" method="post" onSubmit={this.handleSubmit}>
-                  <input className="center" type="text" name="movie[name]" placeholder="Name" value={this.state.movieValue} onChange={this.handleChange}/>
-                  <input className="center" type="text" name="movie[year]" placeholder="Year" value={this.state.yearValue} onChange={this.handleChange} />
-                  <input className=" center btn waves-effect waves-light pink accent-2 " type="submit" value="Add Movie" />
-              </form>
-          </main>
+    return (
+      <div className="App">
+        <nav className="pink accent-2">
+          <div className="nav-wrapper">
+            <a href="/" className="brand-logo center">Netflix, Etc. & Chill</a>
+              <ul className="right hide-on-med-and-down">
+                <li>
+                  <a href="/create ">Create</a>
+                </li>
+              </ul>
+          </div>
+        </nav>
+        <main>
+
+          <form action="/results" method="get">
+            <input type="text" placeholder="Search" className="input" />
+            <button className="button">Submit</button>
+          </form>
+
+            {movies}
+
+          <h4 className="center">Add a Movie!</h4>
+
+          <form className="center" action="/movies" method="post" onSubmit={this.handlePost}>
+            <input className="center" type="text" name="movieValue" placeholder="Name" value={this.state.movieValue} onChange={this.handleChange} />
+            <input className="center" type="text" name="yearValue" placeholder="Year" value={this.state.yearValue} onChange={this.handleChange} />
+            <input className=" center btn waves-effect waves-light pink accent-2 " type="submit" value="Add Movie" />
+          </form>
+
+        </main>
       </div>
-  }
+    )
+    }
 }
 
 export default App
