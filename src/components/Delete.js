9b2d08f6a3485dce4db7b0bb813dcb224a1ffc17
Kathy145 <Kathy.Lambert@LotusDesignsAgency.com>
2018-01-30 17:27:34 -0500

    Updated results and delete components

---
+++ src/components/Delete.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+import './App.css'
+import axios from 'axios'
+
+class Delete extends Component {
+    state = {
+        movies: [],
+        newMovie: {
+            movieValue: '',
+            yearValue: ''
+        }
+    }
+
+    render() {
+      <div className='deleteButton'>
+      </div>
+    }
+}
+
+export default Delete
+
