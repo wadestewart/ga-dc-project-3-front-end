684a761af591f146cf57f68b1c0d479e712c8b05
Kathy145 <Kathy.Lambert@LotusDesignsAgency.com>
2018-01-30 11:02:16 -0500

    Updated App.js to include read functionality

--- src/App.js
+++ src/App.js
@@ -1,28 +1,56 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import axios from 'axios'
 
 class App extends Component {
+  state = {
+        movies: []
+    }
+componentDidMount () {
+	axios.get('https://sheltered-dawn-94402.herokuapp.com/movies')
+    .then(response => {this.setState({ movies: response.data })
+    console.log(response)})
+}
   render() {
-    return (
-      <div className="App">
-        <nav className="pink accent-2">
+    let movies = this.state.movies.map((movie, index) => {
+      console.log(this.state.movies)
+        return <div className='center'>
+                {/* <li key={index} className="movie-name">
+                    <a href={'/movie/' + movie.name}>{movie.name}</a>
+                </li> */}
+                <h4 className="movie-name">{movie.name}</h4>
+                <p className="movie-year">{movie.year}</p>
+                <p className="movie-provider">{movie.provider}</p>
+            </div>
+    })
+    return <div className="App">
+            <nav className="pink accent-2">
                 <div className="nav-wrapper">
-                <a href="/" className="brand-logo center">Netflix, Etc. & Chill</a>
-                <ul className="right hide-on-med-and-down">
-                <li>
-                    <a href="/create ">Create</a>
-                </li>
-                </ul>
+                    <a href="/" className="brand-logo center">
+                        Netflix, Etc. & Chill
+                    </a>
+                    <ul className="right hide-on-med-and-down">
+                        <li>
+                            <a href="/create ">Create</a>
+                        </li>
+                    </ul>
                 </div>
             </nav>
-        <main>
-          <form action="/results" method='get'>
-            <input type="text" placeholder='Search' className='input'/><button className='button'>Submit</button>
-          </form>
-        </main>
-      </div>
-    );
+            <main>
+                <form action="/results" method="get">
+                    <input type="text" placeholder="Search" className="input" />
+                    <button className="button">Submit</button>
+                </form>
+                {movies}
+                <h4 className="center">Add a movie!</h4>
+
+                <form className="center" action="/movies" method="post">
+                    <input className="center" type="text" name="movie[name]" placeholder="Name" />
+                    <input className="center" type="text" name="movie[year]" placeholder="Year" />
+                    <input className=" center btn waves-effect waves-light pink accent-2 " type="submit" value="Add Movie" />
+                </form>
+            </main>
+        </div>
   }
 }
 
