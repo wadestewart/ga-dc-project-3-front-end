132d6147aa05a04de07a3a2c70a34b56f39f4f5f
Wade Stewart <stew.wade@gmail.com>
2018-01-30 11:38:42 -0500

    Styling code

--- src/App.js
+++ src/App.js
@@ -8,11 +8,13 @@ class App extends Component {
         movieValue: '',
         yearValue: ''
     }
+
 componentDidMount () {
 	axios.get('https://sheltered-dawn-94402.herokuapp.com/movies')
     .then(response => {this.setState({ movies: response.data })
     console.log(response)})
 }
+
 handleChange = (event) => {
     this.setState({ movieValue: event.target.movieValue })
     this.setState({ yearValue: event.target.yearValue })
@@ -32,45 +34,46 @@ handleSubmit = (event) => {
 
         })
 }
-  render() {
-    let movies = this.state.movies.map((movie, index) => {
-        return <div className='center'>
-                {/* <li key={index} className="movie-name">
-                    <a href={'/movie/' + movie.name}>{movie.name}</a>
-                </li> */}
-                <h4 className="movie-name">{movie.name}</h4>
-                <p className="movie-year">{movie.year}</p>
-                <p className="movie-provider">{movie.provider}</p>
-            </div>
-    })
-    return <div className="App">
-            <nav className="pink accent-2">
-                <div className="nav-wrapper">
-                    <a href="/" className="brand-logo center">
-                        Netflix, Etc. & Chill
-                    </a>
-                    <ul className="right hide-on-med-and-down">
-                        <li>
-                            <a href="/create ">Create</a>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
-            <main>
-                <form action="/results" method="get">
-                    <input type="text" placeholder="Search" className="input" />
-                    <button className="button">Submit</button>
-                </form>
-                {movies}
-                <h4 className="center">Add a movie!</h4>
 
-                <form className="center" action="/movies" method="post" onSubmit={this.handleSubmit}>
-                    <input className="center" type="text" name="movie[name]" placeholder="Name" value={this.state.movieValue} onChange={this.handleChange}/>
-                    <input className="center" type="text" name="movie[year]" placeholder="Year" value={this.state.yearValue} onChange={this.handleChange} />
-                    <input className=" center btn waves-effect waves-light pink accent-2 " type="submit" value="Add Movie" />
-                </form>
-            </main>
-        </div>
+render() {
+  let movies = this.state.movies.map((movie, index) => {
+      return <div className='center'>
+              {/* <li key={index} className="movie-name">
+                  <a href={'/movie/' + movie.name}>{movie.name}</a>
+              </li> */}
+              <h4 className="movie-name">{movie.name}</h4>
+              <p className="movie-year">{movie.year}</p>
+              <p className="movie-provider">{movie.provider}</p>
+          </div>
+})
+  return <div className="App">
+          <nav className="pink accent-2">
+              <div className="nav-wrapper">
+                  <a href="/" className="brand-logo center">
+                      Netflix, Etc. & Chill
+                  </a>
+                  <ul className="right hide-on-med-and-down">
+                      <li>
+                          <a href="/create ">Create</a>
+                      </li>
+                  </ul>
+              </div>
+          </nav>
+          <main>
+              <form action="/results" method="get">
+                  <input type="text" placeholder="Search" className="input" />
+                  <button className="button">Submit</button>
+              </form>
+              {movies}
+              <h4 className="center">Add a movie!</h4>
+
+              <form className="center" action="/movies" method="post" onSubmit={this.handleSubmit}>
+                  <input className="center" type="text" name="movie[name]" placeholder="Name" value={this.state.movieValue} onChange={this.handleChange}/>
+                  <input className="center" type="text" name="movie[year]" placeholder="Year" value={this.state.yearValue} onChange={this.handleChange} />
+                  <input className=" center btn waves-effect waves-light pink accent-2 " type="submit" value="Add Movie" />
+              </form>
+          </main>
+      </div>
   }
 }
 
