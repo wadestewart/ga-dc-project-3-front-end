99f3e76d798766d5d9ed167c88dd14b73a093411
Savannah Cody <savannah.m.cody@gmail.com>
2018-02-01 16:31:11 -0500

    improved create and add movie buttons

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ const Header = () => (
             <a href="/" className="brand-logo responsive-image red-text z-depth-5 black">Netflix & Chill</a>
                 <ul className="right hide-on-med-and-down">
                     <li>
-                        <a class="red accent-4 waves-effect waves" href="/create">Create</a>
+                        <a class="waves-effect waves red accent-4" href="/create">Create</a>
                     </li>
                 </ul>
         </div>
