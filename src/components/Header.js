ac3a7630679f61583af6f3e1bbc652396155b766
Savannah Cody <savannah.m.cody@gmail.com>
2018-02-01 11:19:42 -0500

    changed font to netflix font specifically

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
 const Header = () => (
-    <nav className="blue-grey lighten-4 z-depth-3">
+    <nav className="white z-depth-3">
         <div className="nav-wrapper">
-            <a href="/" className="brand-logo red-text z-depth-5 black">Netflix, Etc. & Chill</a>
+            <a href="/" className="brand-logo red-text z-depth-5 black">Netflix & Chill</a>
                 <ul className="right hide-on-med-and-down">
                     <li>
                         <a class="red accent-4 waves-effect waves" href="/create">Create</a>
