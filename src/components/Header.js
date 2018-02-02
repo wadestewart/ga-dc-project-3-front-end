3096c8e059362cb2b2eb3a2613d829ba77f80261
Savannah Cody <savannah.m.cody@gmail.com>
2018-02-01 22:36:28 -0500

    figured out how to fix name font color

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Header = () => (
     <nav className="z-depth-3">
         <div className="nav-wrapper">
-            <a href="/" className="brand-logo responsive-image red-text z-depth-5 black">Netflix & Chill</a>
+            <a href="/" className="brand-logo responsive-image z-depth-5 black">Netflix & Chill</a>
                 <ul className="right hide-on-med-and-down">
                     <li>
                         <a class="waves-effect waves red accent-4" href="/create">Create</a>
